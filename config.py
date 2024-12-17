1f3a1419399b2134e89638ea242be5e6ebb75d71
Botboty282 <singhjashan694444@gmail.com>
2024-12-17 10:02:26 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @SomsPvt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7213454875:AAEXof8gTqm6u_Q309xfDC_nrab6VF-jVkg")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("JASHANBRAR001")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7327839772")
+
+
+
+
+
+
+
+  
